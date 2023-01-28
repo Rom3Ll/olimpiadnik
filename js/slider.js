614056f2fc2d35e24abc41994f7bdaf45e196d05
Rom3Ll <113060181+Rom3Ll@users.noreply.github.com>
2023-01-28 18:36:48 +0300

    main page

---
+++ js/slider.js
@@ -0,0 +1,26 @@
+$(document).ready(function (){
+    $(".slider").slick({
+        arrows: true,
+        adaptiveHeight: true,
+        infinite: true,
+        slidesToScroll: 1,
+        speed: 1000,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        initialSlide:0, 
+    });
+});
+
+$(document).ready(function (){
+    $(".slider_nearest").slick({
+      arrows: true,
+      adaptiveHeight: true,
+      infinite: true,
+      slidesToScroll: 1,
+      speed: 1000,
+      autoplay: true,
+      autoplaySpeed: 3000,
+      initialSlide: 0,
+      slidesToShow: 4,
+    });
+  })
